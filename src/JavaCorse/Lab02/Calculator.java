1f938f0600805c1fb78cd8de61e000ef843e5531
AlenaGruzdeva2285 <116582154+AlenaGruzdeva2285@users.noreply.github.com>
2023-03-04 21:24:24 +0300

    Исправила замечания

--- src/JavaCorse/Lab02/Calculator.java
+++ src/JavaCorse/Lab02/Calculator.java
@@ -13,12 +13,9 @@ public class Calculator {
     public double division(double a1, double a2){
         return a1 / a2;
     }
-/*    public long division(long a1, long a2){
-        return a1 / a2;
-    }*/
-/*    public int division(int a1, int a2){
-        return a1 / a2;
-    }*/
+    public double division(long a1, long a2){
+    return (double) a1 / a2;
+           }
     public double multi(double a1, double a2){
         return a1 * a2;
     }
