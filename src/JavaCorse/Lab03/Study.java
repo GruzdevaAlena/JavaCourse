0dd3478f1d4a5d22aaaf8a6813682315bbea06f8
AlenaGruzdeva2285 <116582154+AlenaGruzdeva2285@users.noreply.github.com>
2023-03-05 21:10:37 +0300

    На проверку 3 задание

---
+++ src/JavaCorse/Lab03/Study.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package JavaCorse.Lab03;
+
+public class Study {
+    private String course;
+
+    public Study(String course) {
+        this.course = course;
+    }
+
+    public String printCourse() {
+        return this.course;
+    }
+}
+
