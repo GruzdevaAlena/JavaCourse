8420ced91468eaf8bec5bb936ef9dc7780e527f3
AlenaGruzdeva2285 <116582154+AlenaGruzdeva2285@users.noreply.github.com>
2023-03-18 13:36:02 +0300

    На проверку 9 задание

---
+++ src/JavaCorse/Lab09/Task3/Main.java
@@ -0,0 +1,29 @@
+package JavaCorse.Lab09.Task3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<User, Integer> users = new HashMap<User, Integer>();
+        users = generateMap(users);
+
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        for (Map.Entry<User, Integer> item : users.entrySet()) {
+            if(item.getKey().getName().equals(name)){
+                System.out.println(name + " имеет " + item.getValue() + " очков.");
+            }
+        }
+    }
+    public static Map<User,Integer> generateMap(Map<User,Integer> users){
+        users.put(new User("Анастасия"), 15);
+        users.put(new User("Олег"), 12);
+        users.put(new User("Павел"), 27);
+        users.put(new User("Регина"), 30);
+        users.put(new User("Иван"), 25);
+        return users;
+    }
+}
+
